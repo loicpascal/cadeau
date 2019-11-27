87bb44d9f5b9e1b0cbf65ac7208958cabb552c83
Loic PASCAL <loic.pascal@gmail.com>
2019-11-27 20:05:12 +0100

    change modal alert

--- public/js/mlpn.js
+++ public/js/mlpn.js
@@ -1,7 +1,11 @@
 mlpn = {
 
+    // Durée du cookie pour l'alert en home (en jours)
+    alertCookieDuration: 7,
+
     // Initialisation du site
-    init: function(){
+    init: function()
+    {
 
         // Initialisation des éléments Boostrap tooltip
         this.initBootstrapTooltip();
@@ -10,7 +14,24 @@ mlpn = {
     /**
      * Initialisation des éléments Bootstrap Tooltip
      */
-    initBootstrapTooltip: function() {
+    initBootstrapTooltip: function()
+    {
         $('[data-toggle="tooltip"]').tooltip();
+    },
+
+    /**
+     * Initialisation de l'alerte modal
+     */
+    initBootstrapModalAlert: function()
+    {
+        modalAlert = $('#modalAlert');
+        modalAlert.modal('show');
+
+        modalAlert.on('hidden.bs.modal', function (e) {
+            var date = new Date();
+            date.setDate(date.getDate() + 7);
+            document.cookie = "hideAlert=true;expires=" + date.toUTCString();
+            console.log(date.toUTCString());
+        })
     }
 };
